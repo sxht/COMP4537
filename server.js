5d910ec25e3859e59d7f5a74a80b5376423149ed
sxht <31171951+sxht@users.noreply.github.com>
2025-02-02 21:21:17 -0800

    Testing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const utils = require('./COMP4537/labs/3/modules/utils');
 const en = require('./COMP4537/labs/3/lang/en/en');
 
 http.createServer(function (req, res) {
+    console.log("connected")
     const q = url.parse(req.url, true);
     const name = q.query.name;
     const message = `<p style="color: blue">${en.message.replace("%1", name)} ${utils.getDate()}</p>`;
