32b61ebde516035adb3c2689d66a2303434977ea
sxht <31171951+sxht@users.noreply.github.com>
2025-02-06 12:41:20 -0800

    changed en.js to properly export

--- COMP4537/labs/4/lang/en/en.js
+++ COMP4537/labs/4/lang/en/en.js
@@ -1,3 +1,3 @@
-const notFound = "You illiterate imbicile, this is not defined!!";
-const alreadyExist = "Warning! %1 already exists."
-const addSuccess = "Request# %1 updated on %2. Total Entries: %3."
+module.exports.notFound = "You illiterate imbicile, this is not defined!!";
+module.exports.alreadyExist = "Warning! %1 already exists."
+module.exports.addSuccess = "Request# %1 updated on %2. Total Entries: %3."
